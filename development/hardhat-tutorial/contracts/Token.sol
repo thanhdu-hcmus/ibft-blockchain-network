0ab7b5a7b2783277cf0bb4205cd7054711dc1456
thanhdu <thanhdu-hcmus@gmail.com>
2025-01-08 16:23:58 +0700

    Disable console.log

--- development/hardhat-tutorial/contracts/Token.sol
+++ development/hardhat-tutorial/contracts/Token.sol
@@ -4,7 +4,7 @@
 // It will be used by the Solidity compiler to validate its version.
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 
 
 // This is the main building block for smart contracts.
@@ -48,12 +48,12 @@ contract Token {
         // transaction will revert.
         require(balances[msg.sender] >= amount, "Not enough tokens");
 
-        console.log(
-            "Transferring from %s to %s %s tokens",
-            msg.sender,
-            to,
-            amount
-        );
+        // console.log(
+        //     "Transferring from %s to %s %s tokens",
+        //     msg.sender,
+        //     to,
+        //     amount
+        // );
 
         // Transfer the amount.
         balances[msg.sender] -= amount;
