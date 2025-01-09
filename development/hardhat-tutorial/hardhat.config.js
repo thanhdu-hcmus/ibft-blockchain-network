4c24d5c7f774329d260228b37742f2f659d88b85
thanhdu <thanhdu-hcmus@gmail.com>
2025-01-09 15:26:35 +0700

    Add env vars for config files

--- development/hardhat-tutorial/hardhat.config.js
+++ development/hardhat-tutorial/hardhat.config.js
@@ -1,11 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
+
+// Accessing environment variables
+const URL = process.env.URL; // Accessing a custom variable
+const ACCOUNTS = process.env.ACCOUNTS;  // Accessing the secret
+
 module.exports = {
   solidity: "0.8.27",
   networks: {
     quorum: {
-      url: "http://18.138.252.0:8545",
-      accounts: ["0x13d70c66406b3e631fe9fa24e9ab6136a554769d23229ebaac70e953de389961"],
+      url: URL,
+      accounts: ACCOUNTS.split(','),
     },
   },
 };
