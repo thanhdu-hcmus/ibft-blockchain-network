fa5210fa1f9c4281bb0c1f0c31d087a8d89a8e42
thanhdu <thanhdu-hcmus@gmail.com>
2025-01-09 10:20:11 +0700

    Update pubIp for node-0

--- development/hardhat-tutorial/hardhat.config.js
+++ development/hardhat-tutorial/hardhat.config.js
@@ -4,7 +4,7 @@ module.exports = {
   solidity: "0.8.27",
   networks: {
     quorum: {
-      url: "http://3.0.25.119:8545",
+      url: "http://18.138.252.0:8545",
       accounts: ["0x13d70c66406b3e631fe9fa24e9ab6136a554769d23229ebaac70e953de389961"],
     },
   },
